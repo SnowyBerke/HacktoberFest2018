d66eb3f61742dc41aa45aeb776edcd19fbccbef2
SnowyBerke <56930729+SnowyBerke@users.noreply.github.com>
2019-10-23 22:19:45 +0300

    Update HelloGitHub.java

--- Java/HelloGitHub.java
+++ Java/HelloGitHub.java
@@ -3,7 +3,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 
 	    System.out.println("Hello, World!");
-	    
+	    System.out.println("Hello, GitHub!");
 	    //Yukarıdaki komutun sözdizimini kullanarak aşağıya "Hello, GitHub!" yazdırın.
 
 	}
